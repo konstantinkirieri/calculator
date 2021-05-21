28d505fd8e63f1790ea0c1c5a1e293f4c595d900
konstantinkirieri <kirieris@mail.ru>
2021-05-21 14:02:40 +0300

    home work 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Калькулятор</h1>
+    <div class="calc">
+      <div v-show="!end" class="inputs">
+        <input type="text" v-model="firstValue">
+        <input type="text" v-model="secondValue">
+      </div>
+      <div v-show="end" class="answer">
+        <button @click="end = !end"><i class="fas fa-undo-alt"></i></button>
+        <span>{{ result }}</span>
+      </div>
+      <div class="actions">
+        <button class="action" @click="plus"><i class="fas fa-plus"></i></button>
+        <button class="action" @click="minus"><i class="fas fa-minus"></i></button>
+        <button class="action" @click="division"><i class="fas fa-divide"></i></button>
+        <button class="action" @click="mult"><i class="fas fa-times"></i></button>
+        <button class="action" @click="exp"><i class="fas fa-superscript"></i></button>
+        <button class="action" @click="divisionWithoutRem"><i class="fas fa-divide"></i><br>
+        <span>без ост</span></button>
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      firstValue: '',
+      secondValue: '',
+      result: '',
+      action: '',
+      end: false
+    }
+  },
+  methods: {
+    plus() {
+      this.result = +this.firstValue + +this.secondValue;
+      this.end = true;
+    },
+    minus() {
+      this.result = +this.firstValue - +this.secondValue;
+      this.end = true;
+    },
+    mult() {
+      this.result = +this.firstValue * +this.secondValue;
+      this.end = true;
+    },
+    division() {
+      this.result = +this.firstValue / +this.secondValue;
+      this.end = true;
+    },
+    exp() {
+      this.result = (+this.firstValue) ** +this.secondValue;
+      this.end = true;
+    },
+    divisionWithoutRem() {
+      this.result = Math.floor(+this.firstValue / +this.secondValue);
+      this.end = true;
+    }
+  },
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './style/style.scss'
 
 Vue.config.productionTip = false
 
