26ed8824e68136db8eca605672fcdab7bbf5443d
konstantinkirieri <kirieris@mail.ru>
2021-06-07 10:38:10 +0300

    home work 2

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,15 @@
   <div id="app">
     <h1>Калькулятор</h1>
     <div class="calc">
+      <div style="display: none">
+          <input type="radio" id="one" value='one' v-model="picked">
+          <label for="one">Операнд 1</label> 
+          <input type="radio" id="two" value='two' v-model="picked">
+          <label for="two">Операнд 2</label>       
+        </div>
       <div v-show="!end" class="inputs">
-        <input type="text" v-model="firstValue">
-        <input type="text" v-model="secondValue">
+        <input type="text" @click="changeInputOne" v-model="value.one">
+        <input type="text" @click="changeInputTwo" v-model="value.two">
       </div>
       <div v-show="end" class="answer">
         <button @click="end = !end"><i class="fas fa-undo-alt"></i></button>
@@ -18,8 +24,18 @@
         <button class="action" @click="exp"><i class="fas fa-superscript"></i></button>
         <button class="action" @click="divisionWithoutRem"><i class="fas fa-divide"></i><br>
         <span>без ост</span></button>
+        <button 
+          v-show="checked" 
+          v-for="(button, item) in buttons" 
+          @click="addNumber(button)"
+          :key="item">
+            {{ button }}
+          </button>
+        <button @click="remove" v-show="checked"><i class="fas fa-backspace"></i></button>
+        <button @click="clearInput" v-show="checked"><i class="fas fa-ban"></i></button>
+        <input class="numberButton" type="checkbox" id="checkbox" v-model="checked">
+        <label class="keyboard" for="checkbox"><i class="far fa-keyboard"></i></label>
       </div>
-      
     </div>
   </div>
 </template>
@@ -29,29 +45,50 @@
 export default {
   data() {
     return {
-      firstValue: '',
-      secondValue: '',
+      value: {
+        'one': '',
+        'two': '',
+      },
       result: '',
       action: '',
-      end: false
+      end: false,
+      checked: false,
+      buttons: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+      picked: true,
     }
   },
   methods: {
+    clearInput() {
+      this.value.one = '';
+      this.value.two = '';
+    },
+    changeInputOne() {
+      this.picked = 'one';
+    },
+    changeInputTwo() {
+      this.picked = 'two';
+    },
+    remove() {
+      this.value[this.picked] = this.value[this.picked].slice(0, this.value[this.picked].length - 1)
+    },
+    addNumber(num) {
+      this.value[this.picked] = this.value[this.picked] + num;
+    },
     plus() {
-      this.result = +this.firstValue + +this.secondValue;
+      this.result = +this.value.one + +this.value.two;
       this.end = true;
     },
     minus() {
-      this.result = +this.firstValue - +this.secondValue;
+      this.result = +this.value.one - +this.value.two;
       this.end = true;
     },
     mult() {
-      this.result = +this.firstValue * +this.secondValue;
+      this.result = +this.value.one * +this.value.two;
       this.end = true;
     },
     division() {
-      if(this.secondValue != false) {
-        this.result = +this.firstValue / +this.secondValue;
+      if(this.value.two != false) {
+        this.result = +this.value.one / +this.value.two;
       } else {
         this.result = 'На 0 делить нельзя'
       }
@@ -59,12 +96,12 @@ export default {
       this.end = true;
     },
     exp() {
-      this.result = (+this.firstValue) ** +this.secondValue;
+      this.result = (+this.value.one) ** +this.value.two;
       this.end = true;
     },
     divisionWithoutRem() {
-      if(this.secondValue != false) {
-        this.result = Math.floor(+this.firstValue / +this.secondValue);
+      if(this.value.two != false) {
+        this.result = Math.floor(+this.value.one / +this.value.two);
       } else {
         this.result = 'На 0 делить нельзя'
       }
